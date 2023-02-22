71443218e7d84ab3d1a7b8be8da01c361c8612b4
John Szaniszlo <Johnszaniszlo@gmail.com>
2023-02-21 17:17:50 -0800

    telescope commands

--- lua/bindings.lua
+++ lua/bindings.lua
@@ -15,7 +15,45 @@ keymap('n', '<Leader>f', _find_files)
 keymap('n', '<Leader>o', '<cmd> Telescope oldfiles <cr>')
 keymap('n', '<Leader>s', '<cmd> Telescope live_grep <cr>')
 
+-- Telescope commands
+local commands_table = {
+  Tff = 'find_files',
+  Tof = 'oldfiles',
+  Tlg = 'live_grep',
+  Tsh = 'search_history',
+  Tqf = 'quickfix',
+  Tvo = 'vim_options',
+  Tss = 'spell_suggest',
+  Treg = 'registers',
+  Tcmd = 'commands',
+  Thlp = 'help_tags',
+  Tman = 'man_pages',
+  Tkey = 'keymaps',
+  Tcol = 'colorscheme',
+  Tacmd = 'autocommands',
+  Tgstr = 'grep_string',
+  Tlrf = 'lsp_references',
+  Tlds = 'lsp_document_symbols',
+  Tlws = 'lsp_workspace_symbols',
+  Tlca = 'lsp_code_actions',
+  Tltd = 'lsp_type_definitions',
+  Tld = 'diagnostics',
+  Tli = 'lsp_implementations',
+  Tld = 'lsp_definitions',
+  Tgc = 'git_commits',
+  Tgb = 'git_branches',
+  Tgs = 'git_status',
+  Tgf = 'git_files',
+}
+
+do
+  for cmd, act in pairs(commands_table) do
+    vim.cmd('command! ' .. cmd .. ' :Telescope ' .. act)
+  end
+end
+
 -- bufferline
+
 keymap('n', '<tab>', '<cmd> BufferLineCycleNext <cr>')
 keymap('n', '<s-tab>', '<cmd> BufferLineCyclePrev <cr>')
 keymap('n', 'm.', '<cmd> BufferLineMoveNext <cr>')
