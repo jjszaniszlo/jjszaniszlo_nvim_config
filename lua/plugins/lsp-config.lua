f63e3fcd0f1386dd19963c673be47d660264abc4
John Szaniszlo <Johnszaniszlo@gmail.com>
2023-02-21 23:43:21 -0800

    cmake server

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,6 @@
 require 'mason'.setup {}
 require 'mason-lspconfig'.setup {
-  ensure_installed = { 'lua_ls', 'clangd', 'rust_analyzer', 'html', 'cssls', 'tsserver' },
+  ensure_installed = { 'lua_ls', 'clangd', 'rust_analyzer', 'html', 'cssls', 'tsserver', 'cmake' },
 }
 
 local nvim_lsp = require 'lspconfig'
@@ -8,13 +8,17 @@ local nvim_lsp = require 'lspconfig'
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require 'cmp_nvim_lsp'.default_capabilities(capabilities)
 
-local servers = { 'lua_ls', 'clangd', 'rust_analyzer', 'html', 'cssls', 'tsserver' }
+local servers = { 'lua_ls', 'clangd', 'rust_analyzer', 'html', 'cssls', 'tsserver', 'cmake' }
 
 local lsp_format = require 'lsp-format'
 lsp_format.setup {}
 
 do
   for _, lsp in pairs(servers) do
+    local attach_func = lsp_format.on_attach
+    if (lsp == 'cmake') then
+      attach_func = on_attach
+    end
     nvim_lsp[lsp].setup {
       on_attach = lsp_format.on_attach,
       capabilities = capabilities,
